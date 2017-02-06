4544334e58ee891fa5d973acd85feac9d2cb0c0b
Christopher Zenzel <chriszenzel@outlook.com>
2017-02-05 20:10:27 -0500

    Updates for Unity

--- degrib-js/build.sh
+++ degrib-js/build.sh
@@ -8,6 +8,4 @@ emcc -O2 -s ALLOW_MEMORY_GROWTH=1 -o dist/degrib.browser.js --pre-js javascript/
 
 emcc -O2 -s ALLOW_MEMORY_GROWTH=1 -o dist/degrib.node.js --pre-js javascript/pre-node.js src/degrib.bc src/include-fs.bc
 
-emcc -O2 -s ALLOW_MEMORY_GROWTH=1 -o dist/degrib.unity.js --pre-js javascript/pre-unity.js --post-js javascript/post-unity.js src/degrib.bc src/include-fs.bc
-
-emcc -O2 -s ALLOW_MEMORY_GROWTH=1 -o dist/degrib.plain.html src/degrib.bc src/include-fs.bc
+emcc --memory-init-file 0 -s ALLOW_MEMORY_GROWTH=1 -o dist/degrib.unity.js --pre-js javascript/pre-browser.js --post-js javascript/post-browser.js src/degrib.bc src/include-fs.bc
